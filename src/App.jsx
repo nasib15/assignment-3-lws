2356d676dc327e5a74bc7c543b62dd0d302fc3c2
nasib15 <nasibhossain57@gmail.com>
2024-10-22 19:55:06 +0600

    Add:Navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="bg-gray-900 text-white flex h-screen">
       <Sidebar />
+      <main className="flex-1 overflow-y-auto overflow-x-hidden">
+        <Navbar />
+      </main>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { MailSVG, MenuSVG, NotificationSVG } from "./SVG/IconSVG";
+
+const Navbar = () => {
+  return (
+    <>
+      <header className="flex items-center justify-between bg-gray-800 p-4">
+        <button className="lg:hidden">
+          <MenuSVG />
+        </button>
+        <div className="mx-4 flex-1">
+          <input
+            type="text"
+            placeholder="Search here"
+            className="w-full max-w-xl rounded-full bg-gray-700 px-4 py-2 text-white focus:outline-none"
+          />
+        </div>
+        <div className="flex items-center">
+          <button className="relative mr-4">
+            <NotificationSVG />
+            <span className="absolute right-0 top-0 h-2 w-2 rounded-full bg-red-500"></span>
+          </button>
+          <button className="relative mr-4">
+            <MailSVG />
+            <span className="absolute right-0 top-0 h-2 w-2 rounded-full bg-red-500"></span>
+          </button>
+        </div>
+      </header>
+    </>
+  );
+};
+export default Navbar;
